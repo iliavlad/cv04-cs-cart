e4ebb36d1b3d3d1221e329f73e5bde16750af6dd
Гарага Илья Дмитриевич <idgaraga@rt-dc.ru>
2021-09-03 14:33:04 +1000

    Разделение обработки типов товаров по методам

--- src/Shop.php
+++ src/Shop.php
@@ -11,58 +11,170 @@ final class Shop
      */
     private $items;
 
+    /**
+     * Items can have special methods to change its state
+     *
+     * A key => a name of an item, avalue => a function with 'updateItem' prefix
+     *
+     * @var array
+     */
+    private $specialItems = [
+        'Blue cheese'       => 'BlueCheese',
+        'Mjolnir'           => 'Mjolnir',
+        'Concert tickets'   => 'ConcertTickets',
+        'Magic cake'        => 'Magic',
+    ];
+
     public function __construct(array $items)
     {
         $this->items = $items;
     }
 
+    /**
+     * Do items update
+     *
+     * @return void
+     */
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Blue cheese' and $item->name != 'Concert tickets') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Mjolnir') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Concert tickets') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            $this->updateItem($item);
+        }
+    }
 
-            if ($item->name != 'Mjolnir') {
-                $item->sell_in = $item->sell_in - 1;
-            }
+    /**
+     * Update a single item
+     *
+     * @param Item $item
+     * @return void
+     */
+    private function updateItem(Item $item): void
+    {
+        // Option 1
+        $method = 'updateCommonItem';
+        if (isset($this->specialItems[$item->name])) {
+            $method = 'updateItem' . $this->specialItems[$item->name];
+        }
+        $this->$method($item);
 
+        // Option 2
+//        switch ($item->name) {
+//            case 'Blue cheese':
+//                $this->updateItemBlueCheese($item);
+//                break;
+//            case 'Mjolnir':
+//                $this->updateItemMjolnir($item);
+//                break;
+//            case 'Concert tickets':
+//                $this->updateItemConcertTickets($item);
+//                break;
+//            case 'Magic cake':
+//                $this->updateItemMagic($item);
+//                break;
+//            default :
+//                $this->updateCommonItem($item);
+//                break;
+//        }
+    }
+
+    /**
+     * Update an item without special condition
+     *
+     * @param Item $item
+     * @return void
+     */
+    private function updateCommonItem(Item $item): void
+    {
+        $item->sell_in--;
+
+        if ($item->quality > 0) {
+            $qualityDecrement = 1;
             if ($item->sell_in < 0) {
-                if ($item->name != 'Blue cheese') {
-                    if ($item->name != 'Concert tickets') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Mjolnir') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+                $qualityDecrement = 2;
+            }
+            $item->quality -= $qualityDecrement;
+            if ($item->quality < 0) {
+                $item->quality = 0;
+            }
+        }
+    }
+
+    /**
+     * Update an item with special condition 'Blue Cheese'
+     *
+     * @param Item $item
+     * @return void
+     */
+    private function updateItemBlueCheese(Item $item): void
+    {
+        $item->sell_in--;
+
+        if ($item->quality < 50) {
+            $qualityIncrement = 1;
+            if ($item->sell_in < 0) {
+                $qualityIncrement = 2;
+            }
+            $item->quality += $qualityIncrement;
+            if ($item->quality > 50) {
+                $item->quality = 50;
+            }
+        }
+    }
+
+    /**
+     * Update an item with special condition 'Mjolnir'
+     *
+     * @param Item $item
+     * @return void
+     */
+    private function updateItemMjolnir(Item $item): void
+    {
+    }
+
+    /**
+     * Update an item with special condition 'Concert Tickets'
+     *
+     * @param Item $item
+     * @return void
+     */
+    private function updateItemConcertTickets(Item $item): void
+    {
+        $item->sell_in--;
+
+        if ($item->sell_in < 0) {
+            $item->quality = 0;
+        } elseif ($item->quality < 50) {
+            $qualityIncrement = 1;
+            if ($item->sell_in < 5) {
+                $qualityIncrement = 3;
+            } elseif ($item->sell_in < 10) {
+                $qualityIncrement = 2;
+            }
+            $item->quality += $qualityIncrement;
+            if ($item->quality > 50) {
+                $item->quality = 50;
+            }
+        }
+    }
+
+    /**
+     * Update an item with special condition 'Magic'
+     *
+     * @param Item $item
+     * @return void
+     */
+    private function updateItemMagic(Item $item): void
+    {
+        $item->sell_in--;
+
+        if ($item->quality > 0) {
+            $qualityDecrement = 2;
+            if ($item->sell_in < 0) {
+                $qualityDecrement = 4;
+            }
+            $item->quality -= $qualityDecrement;
+            if ($item->quality < 0) {
+                $item->quality = 0;
             }
         }
     }
--- tests/ApprovalTest.php
+++ tests/ApprovalTest.php
@@ -21,4 +21,17 @@ class ApprovalTest extends TestCase
 
         Approvals::verifyString($output);
     }
+
+    public function testTestFixtureWithMagic(): void
+    {
+        $argv[0] = 'texttest_fixture_with_magic.php';
+        $argv[1] = 31;
+
+        ob_start();
+        require_once __DIR__ . '/../fixtures/texttest_fixture_with_magic.php';
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        Approvals::verifyString($output);
+    }
 }
